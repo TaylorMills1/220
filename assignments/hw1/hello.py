55def9b7081949f1bff569045ef98f9c0a842bc4
taylormills <millste@g.cofc.edu>
2021-08-31 18:32:45 -0400

    completed hw 1

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -1,6 +1,13 @@
 """
-Name: Taylor Mills
-hello.py
+Name: <Taylor Mills>
+<hello>.py
+
+Problem: Function prints to display "hello, world!"
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
 """
+
+
 def main():
     print("hello, world!")
