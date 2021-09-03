f3ba8408049e418663d6276703a2c24881bbafbb
taylormills <millste@g.cofc.edu>
2021-09-03 10:45:32 -0400

    completed hw 1

---
+++ assignments/My docstring.py
@@ -0,0 +1,14 @@
+"""
+Name: <Taylor Mills>
+<filename>.py
+
+Problem: <Enter description of the codes function>
+
+Certificate of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def me():
+    print("docstring")
+
