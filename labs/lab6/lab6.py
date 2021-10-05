e37e221b155d01acd861000038ca7f3eda4c1c36
taylormills <millste@g.cofc.edu>
2021-10-05 15:51:23 -0400

    completed hw 4

--- labs/lab6/lab6.py
+++ labs/lab6/lab6.py
@@ -8,6 +8,55 @@ def name_reverse():
     """
     Read a name in first-last order and display it in last-comma-first order.
     """
+    name = input('Enter First and Last Name: ')
+    first, last = name.split()
+    flip = ','.join([last, first])
+    print(flip)
+
+
+def company_name():
+    website = input('Enter three-part domain name: ')
+    weebsite = website.split(".")
+    name = weebsite[2]
+    print("Company name: ", name)
+
+
+def initials():
+    quantity = eval(input("How many names will be entered? "))
+    for _ in range(quantity):
+        first = input('Enter students first name: ')
+        last = input('Enter students last name: ')
+        print(first[0] + last[0])
+
+
+def names():
+    namess = input('Enter list of names separated by commas: ')
+    lists = namess.split(",")
+    for c in namess:
+        namess = lists.split()
+        first = namess[0][0]
+
+
+
+def thirds():
+    sentences = eval(input("How many sentences will be input? "))
+    for i in range(sentences):
+        sentence = input('input sentence: ')
+        print(sentence[::3])
+
+
+def word_average():
+    sentence = input("enter sentence: ")
+    words = sentence.split()
+    average = sum(len(word) for word in words) / len(words)
+    print(average)
+
+
+def pig_latin():
+    words = input("Input Sentence:").split()
+    for word in words:
+        print(word[1:] + word[0] + "ay", end=" ")
+    print()
 
 
 def main():
