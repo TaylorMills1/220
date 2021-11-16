df286b0f179691f5f0e2346ad35b5201b29bf7d6
taylormills <millste@g.cofc.edu>
2021-11-16 15:13:52 -0500

    Lab 11 is only here to make me suffer, but I did what could/

--- labs/lab11/lab11.py
+++ labs/lab11/lab11.py
@@ -1,11 +1,52 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Taylor Mills
+hangman.py
 """
-
-def main():
-    # add other function calls here
-    pass
+import random
+import re
 
 
-main()
+def hangman():
+    with open("wordlist.txt") as f:
+        word_list = f.read().splitlines()
+
+    number_of_lives = 7
+
+    random_num = random.randint(0, len(word_list)-1)
+    word_chosen = word_list[random_num]
+
+    encoded_word = re.sub('abcdefghijklmnopqrstuvwxyz', '-', word_chosen)
+
+    def guess(letter, word, encoded):
+        found = False
+        if letter in word:
+            found = True
+            for i in range(0, len(word)):
+                if word[i] == letter:
+                    encoded = encoded[0:i] + letter + encoded[i+1:len(encoded)]
+            return found, encoded
+    print(encoded_word)
+
+    while number_of_lives > 0:
+        guessed_letter = input("Your guess: ")[:1]
+
+        letter_found, encoded_word = guess(guessed_letter, word_chosen, encoded_word)
+
+        if not letter_found:
+            number_of_lives -= 1
+            if number_of_lives == 0:
+                print("\nLetter not found, game over. The word was %s" % word_chosen)
+                break
+            else:
+                print("\nLetter not found. Tries left: %d" % number_of_lives)
+                print(encoded_word)
+        else:
+            if "-" not in encoded_word:
+                print("\nThe word was '%s'" % word_chosen)
+                break
+            else:
+                print("Letter was found.")
+                print(encoded_word)
+
+
+hangman()
